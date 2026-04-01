f34c781e95986526520a390c11d3fedbeeaa443e
Ngozi <hexwhyzed01@gmail.com>
2026-04-01 09:06:56 +0100

    Build a responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,9 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Grid, GridItem, Stack } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <Button>Click me</Button>
-  )
-}
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"coral"}>
+        Hey1
+      </GridItem>
+      <Stack hideBelow="lg"> {/*This is used for responsiveness. To hide thr aside bar below large screens*/}
+        <GridItem area={"aside"} bg={"gold"}>
+          Hey2
+        </GridItem>
+      </Stack>
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        Hey3
+      </GridItem>
+    </Grid>
+  );
+};
 
-export default App
+export default App;
