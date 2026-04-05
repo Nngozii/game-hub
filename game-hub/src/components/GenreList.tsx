b5133575d689c6dfad4da95fb406a62f8a93b432
Ngozi <hexwhyzed01@gmail.com>
2026-04-05 05:24:54 +0100

    feat: Add onSe;lect genre functionality

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -4,8 +4,11 @@ import api from "../services/api-client";
 import type { Game } from "../../hooks/GameGrid";
 import { List, ListItem, Spinner, Button, Text } from "@chakra-ui/react";
 
+interface Props {
+  onSelectGenre: (genre: string) => void
+}
 
-const GenreList = () => {
+const GenreList = ({onSelectGenre}:Props) => {
   const [genres, setGenres] = useState<string[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -16,6 +19,7 @@ const GenreList = () => {
     api
       .get<Game[]>("/games")
       .then((response) => {
+
         /* Here, uniqueGenre exists because, when I fetch the genres, it list all the genre for each game which means
 duplicates. uniqueGenre uses the Set data type (new Set), like an array but it ignores duplicates. However, it doesn't
 return an array, it returns a string, which is why ...(spread operator) is used to spread them into an
@@ -52,7 +56,7 @@ array in order to be able to map. The :Game is just a typescript thing needed.
             paddingLeft: "18px",
           }}
         >
-          <Button variant={"ghost"} onClick={() => {console.log("CLicked", data)}}>{data}</Button>
+          <Button variant={"ghost"} onClick={() => onSelectGenre(data)}>{data}</Button>
         </ListItem>
       ))}
     </List.Root>
